f2fcba28ceb597640aa2fd4b01a959123678113b
Ranny Erzitha <110460970+rnyztha@users.noreply.github.com>
2022-10-14 18:11:53 +0700

    Add files via upload

---
+++ uts_prakpbo/UTS_PrakPBO.java
@@ -0,0 +1,71 @@
+//Soal 1
+package uts_prakpbo;
+
+/**
+ *
+ * @author 21343013-Ranny Erzitha
+ */
+public class UTS_PrakPBO {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        for (a=1; a<=1; a++)
+        {
+            System.out.print(" ");
+            for (b=2; b<=3; b++)
+                {
+                    System.out.print(b);
+                }
+                    System.out.print("   ");
+                    for (b=4; b<=5; b++)
+                    {
+                        System.out.print(b);
+                    }
+                        System.out.println();
+                            for (b=1; b<=4; b++)
+                                {
+                                    System.out.print(b);
+                                }
+                                    System.out.print(" ");
+                                    for (b=5; b<=8; b++)
+                                {
+                                    System.out.print(b);
+                                }
+                                    System.out.println();
+        }
+
+        for (a=1; a<=1; a++)
+        {
+                for (b=1; b<=9; b++)
+                {
+                    System.out.print(b);
+                }
+                    System.out.println();
+                    System.out.print(" ");
+                        for (b=1; b<=7; b++)
+                            {
+                                System.out.print(b);
+                            }
+                                System.out.println();
+                                System.out.print("  ");
+                                for (b=1; b<=5; b++)
+                                    {
+                                        System.out.print(b);
+                                    }
+                                        System.out.println();
+                                        System.out.print("   ");
+                                            for (b=1; b<=3; b++)
+                                                {
+                                                    System.out.print(b);
+                                                }
+                                                    System.out.println();
+                                                    System.out.print("    ");
+                                                        for (b=1; b<=1; b++)
+                                                        {
+                                                            System.out.print(b);
+                                                        }       
+        }
+    }
+}
